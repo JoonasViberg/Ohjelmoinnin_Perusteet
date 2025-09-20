06a3fa108bd3e200b022389e9f53fe26ec5e40e7
Joonas Viberg <joonas.viberg@gmail.com>
2025-09-20 16:55:29 +0300

    T5 ja T6 tehtävät

---
+++ A1/A1_T5.py
@@ -0,0 +1,20 @@
+#1. Print “Calculate the area of a wall.”
+print ("Calculate the area of a wall.")
+#2. Prompt user
+# 1. “Enter the width in meters: ”
+# 2. Store the input value into Feed variable.
+Feed = input("Enter the width in meters: ")
+#3. Convert the Feed variable into an integer and store it in Width variable
+Width = int(Feed)
+#4. Prompt user
+#“Enter the height in meters: ”
+#store the input value into Feed variable.
+Feed = input("Enter the height in meters: ")
+#Convert the Feed variable into an integer and store it in Height variable
+Height = int(Feed)
+#Print “Width is {Width} m and height is {Height} m.”
+print (f"Width is {Width} m and height is {Height} m.")
+#Multiply Width and Height, then store the result in Area variable
+Area = Width * Height
+#Display results to the user: “The wall will be {Area} square meters.”
+print (f"The wall will be {Area} square meters.")
